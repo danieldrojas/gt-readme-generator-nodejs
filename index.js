d27ee85b4d5c2532a3c77e19efb954d2f44170cd
Daniel Rojas <daniel.drojas@outlook.com>
2020-06-25 22:26:50 -0400

    added format to generateMarkdown function

--- index.js
+++ index.js
@@ -1,16 +1,88 @@
 // array of questions for user
+
+const fs = require('fs')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown');
+const Choice = require('inquirer/lib/objects/choice');
+
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Write a title for your README"
+    },
+    {
+        type: "input",
+        message: "Write a description:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Installation for your project:",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Usage for your project:",
+        name: "usage"
+    },
+    {
+        type: "list",
+        message: "Choose a license for your project:",
+        name: "license",
+        choices: ["MIT", "BSD", "CDDL", "EPL"]
+    },
+    {
+        type: "input",
+        message: "Contribution in your project",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "Test for your project:",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "Email: ",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Github: ",
+        name: "github"
+    }
+
 
 ];
+inquirer.prompt(questions)
+.then(function (data) {
+        
+       const fileName = 'sampleREADME.md';
+        
+        writeToFile(fileName, data)
+    
+    
+    
+})
 
-// function to write README file
+
+//Write README function
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data, null, '\t'), function (err) {
+        if (err) {
+            return console.log(err)
+        }
+
+        console.log("Success")
+    })
 }
 
-// function to initialize program
+
+// // function to initialize program
 function init() {
 
 }
 
-// function call to initialize program
-init();
+// // function call to initialize program
+// init()
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,55 @@
 // function to generate markdown for README
+
+
+
+
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `  
+  # ${data.title}
+
+  # Description
+
+  ${data.description}
+
+  ![License](https://img.shields.io/badge/License-${data.license}-red)
+
+  ## Table of Content 
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [MIT License](#MIT-License)
+* [Contributing](#Contributing)
+
+
+## Installation
+
+${data.installation}
+
+## Usage 
+
+${data.usage}
+
+
+## License 
+
+${data.license}
+
+
+## Contributing 
+
+${data.contributing}
+
+
+## Test 
+
+${data.test}
+
+
+## Question
+
+${data.email}
+
+${data.github}
 
 `;
 }
